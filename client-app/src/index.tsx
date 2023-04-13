e8f02adb1e4805bae639dab643111bfad7b4bae2
Sander5151 <sandervanderdussen1@gmail.com>
2023-04-13 02:29:22 +0200

    End of section 7

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 import './app/layout/styles.css';
 import App from './app/layout/App';
+import { StoreContext, store } from './app/stores/store';
  
 ReactDOM.render(
-    <App />,
+  <StoreContext.Provider value={store}>
+    <App />
+  </StoreContext.Provider>,
   document.getElementById('root')
 );
